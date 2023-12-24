afa1f7fbf343d52529b65c4bd217ba27a16cc5d7
Salvador Oliveira <slsalvadorlucas@gmail.com>
2023-12-24 15:31:59 -0400

    :sparkles: Add asdf

--- initial_setup.sh
+++ initial_setup.sh
@@ -7,6 +7,11 @@ tldr -u
 # Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+echo '. "$HOME/.asdf/asdf.sh"' >> ~/.zshrc
+omz reload
+
 # Install vscode
 curl -L 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -o vscode.deb
 sudo dpkg -i vscode.deb
