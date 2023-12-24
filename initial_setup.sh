f376d22a27a03927be10bb9dd2255c9216d283e9
Salvador Oliveira <slsalvadorlucas@gmail.com>
2023-12-24 15:29:52 -0400

    :sparkles: Add ohmyzsh

--- initial_setup.sh
+++ initial_setup.sh
@@ -4,6 +4,9 @@ sudo apt install -y git curl tldr zsh
 # Update tldr database
 tldr -u
 
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install vscode
 curl -L 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -o vscode.deb
 sudo dpkg -i vscode.deb
