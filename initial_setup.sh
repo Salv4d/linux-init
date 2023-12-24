bb41c42461f3241cf7955af649252b56b1c7b73b
Salvador Oliveira <slsalvadorlucas@gmail.com>
2023-12-24 15:22:39 -0400

    :sparkles: Add vscode install

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,3 +1,6 @@
 sudo apt install git curl tldr
 tldr -u
 
+# Install vscode
+curl -L 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -o vscode.deb
+sudo dpkg -i vscode.deb
